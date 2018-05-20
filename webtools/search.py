6dd6ae2871c3e05277c393fbb509316da66dbe2d
Scott Bradley <scottbradley.mail@gmail.com>
2018-05-20 09:42:37 -0500

    adds bing web page object with full-snippet and rich caption description extraction

--- webtools/search.py
+++ webtools/search.py
@@ -3,6 +3,29 @@ import requests
 import time
 
 
+class BingWebPage(dict):
+
+    """
+    No real attempt is made to stylize or format the full snippet or full rich
+    caption (below). These are primarily useful for preliminary inspection of
+    web page content, not so much for e.g. user interface display
+    """
+    def get_full_snippet(self):
+        snippets = [link.get('snippet', '').strip() for link in
+            self.get('deepLinks',[]) if link.get('snippet', '').strip()]
+        return '\n'.join([self.get('snippet', '')] + snippets)
+
+    """
+    The richCaption parameter does not seem to be documented in Azure
+    documentation. Use with caution.
+    """
+    def get_full_richcaption_description(self):
+        descriptions = [section.get('description', '').strip() for section in
+            self.get('richCaption', {}).get('sections', [])
+            if section.get('description', '').strip()]
+        return '\n'.join(descriptions)
+
+
 class BingSearcher(object):
 
     SEARCH_URL = "https://api.cognitive.microsoft.com/bing/v7.0/search"
@@ -11,11 +34,13 @@ class BingSearcher(object):
         self._api_key = bing_api_key
         self._rate_limit = rate_limit # calls per second
         self._rate = 1 / rate_limit
-        self._last_search = None
+        self._last_search_time = None
+        self._last_results = {}
 
     def search(self, query, count=50, offset=0):
-        if self._last_search is not None:
-            delta = (datetime.datetime.now() - self._last_search).total_seconds()
+        if self._last_search_time is not None:
+            now = datetime.datetime.now()
+            delta = (now - self._last_search_time).total_seconds()
             delay = self._rate - delta
             if delay > 0:
                 time.sleep(delay)
@@ -29,6 +54,11 @@ class BingSearcher(object):
         }
         response = requests.get(self.SEARCH_URL, headers=headers, params=params)
         response.raise_for_status()
-        self._last_search = datetime.datetime.now()
-        return response.json()
+        self._last_search_time = datetime.datetime.now()
+        data = response.json()
+        self._last_results = data
+        return data
 
+    def last_search_pages(self):
+        return (BingWebPage(page) for page in
+            self._last_results.get('webPages', {}).get('value', []))
